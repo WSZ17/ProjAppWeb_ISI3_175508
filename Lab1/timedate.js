7cac5d0a974860b650507a98e284b6748430c725
WSZ17 <147032468+WSZ17@users.noreply.github.com>
2025-10-09 16:21:19 +0200

    Add files via upload

--- Lab1/timedate.js
+++ Lab1/timedate.js
@@ -1,7 +1,7 @@
 function gettheDate()
 {
 	Todays = new Date();
-	TheDate = "" + (Todays.getMonth()+ 1) + " / "+ Todays.getDate() + " / " + (Todays.getYear()-100);
+	TheDate = "" + Todays.getDate() + " / " + (Todays.getMonth()+ 1) + " / " + (Todays.getYear()-100);
 	document.getElementById("data").innerHTML = TheDate;
 }
 
@@ -28,10 +28,9 @@ function showtime()
 	var hours = now.getHours();
 	var minutes = now.getMinutes();
 	var seconds = now.getSeconds()
-	var timeValue = "" + ((hours > 12) ? hours -12 :hours)
+	var timeValue = "" + hours
 	timeValue += ((minutes < 10) ? ":0" : ":") + minutes
 	timeValue += ((seconds < 10) ? ":0" : ":") + seconds
-	timeValue += (hours >= 12) ? " P.M." : " A.M."
 	document.getElementById("zegarek").innerHTML = timeValue;
 	timerID = setTimeout("showtime()",1000);
 	timerRunning = true;
