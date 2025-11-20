c8937cd9a3d1eb3d50a1be519a49394845596e27
WSZ17 <147032468+WSZ17@users.noreply.github.com>
2025-11-20 16:28:43 +0100

    Add files via upload

--- Strona/cfg.php
+++ Strona/cfg.php
@@ -4,6 +4,8 @@
 	$dbuser = 'root';
 	$dbpass = '';
 	$baza = 'moja_strona';
+	$login = 'admin_root@localhost.com';
+    $pass = 'ZXCASDqwe123';
 	
 	$link = mysqli_connect($dbhost,$dbuser,$dbpass);
 	if (!$link) echo '<b>przerwane polaczenie</b>';
